db217ee636a1ffb7f88f17a2111660c382e20224
ntk-batch12 <ahmad.noor24@outlook.com>
2024-09-14 20:04:39 -0400

    added method

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -12,4 +12,9 @@ public class DemoDay3 {
 	public static void add(int a, int b) {
 		System.out.println(a+b);
 	}
+	
+	
+	public static void divide(int a, int b) {
+		System.out.println(a/b);
+	}
 }
