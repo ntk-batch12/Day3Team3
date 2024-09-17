7ca4d834903f21fc96a4c92fba83785c18032213
rogef <rogef@Roge-Ram.attlocal.net>
2024-09-17 14:47:53 -0400

    testNew

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -3,52 +3,59 @@ package day3;
 public class DemoDay3 {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Day 3 Team 3");
-		
+
 	}
-	
-	
+
 	public static void add(int a, int b) {
-		System.out.println(a+b);
+		System.out.println(a + b);
 	}
-	
-	
+
 	public static void divide(int a, int b) {
-		System.out.println(a/b);
+		System.out.println(a / b);
 	}
-	
+
 	public static void mustafa() {
 		System.out.println("test");
 	}
-	
-	public static void practice () {
+
+	public static void practice() {
 		System.out.println("Practice3");
 	}
-	public static void tom (int a, int b) {
-		System.out.println(a+b);
+
+	public static void tom(int a, int b) {
+		System.out.println(a + b);
 	}
+
 	public static void multiply(int a, int b) {
-		System.out.println(a*b);
-	}
-		public static void subtract (int b, int c) {
-		System.out.println(b-c);
-	}
-		public static void testTeam3() {
-			System.out.println("Team 3.. we are very good");
-		}
-
-		public static void maysa() {
-			System.out.println("team 3");
-		}
-		public static void maysa2() {
-			System.out.println("m");
-			
-		}
-	public static void newMethodTest(){
-	System.out.println("m");
-
+		System.out.println(a * b);
 	}
 
+	public static void subtract(int b, int c) {
+		System.out.println(b - c);
+	}
+
+	public static void testTeam3() {
+		System.out.println("Team 3.. we are very good");
+	}
+
+	public static void maysa() {
+		System.out.println("team 3");
+	}
+
+	public static void maysa2() {
+		System.out.println("m");
+
+	}
+
+	public static void newMethodTest() {
+		System.out.println("m");
+
+	}
+
+	public void newTest() {
+
+	}
 
 }
